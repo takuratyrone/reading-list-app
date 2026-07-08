1af31fcca088676166ca1b54738f4816ae58496b
takuratyrone <ttkawome@gmail.com>
2026-07-08 09:19:02 +0200

    added safe area to ThemedView component

--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -1,15 +1,31 @@
 import { View, useColorScheme } from 'react-native'
 import React from 'react'
 import { Colours } from '../constants/Colours'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-const ThemedView = ({ style, ...props }) => {
+const ThemedView = ({ style, safe=false, ...props }) => {
     
     const colourScheme = useColorScheme()
     const theme = Colours[colourScheme] ?? Colours.light
 
-    return (
+    if (!safe) return (
         <View 
             style={[{backgroundColor: theme.background}, style]}
+            {...props}
+        />
+    )
+
+    const insets = useSafeAreaInsets()
+
+    return (
+        <View 
+            style={[{
+                backgroundColor: theme.background, 
+                paddingTop: insets.top,
+                paddingBottom: insets.bottom,
+            }, 
+            style
+        ]}
             {...props}
         />
     )
